f92ac28a943113ee86c76ce71701d203fa25bfe9
minseok127 <yms127@naver.com>
2024-05-03 14:52:10 +0900

    fix typo

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$(BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 # fail if tying to reference a variable that is not set
